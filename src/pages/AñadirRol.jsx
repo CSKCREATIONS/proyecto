de574a7469f227000bbaa2dd45ecfbf627d673c7
jiji <klfbkljgfdaj>
2025-06-17 17:23:39 -0500

    Archivos moduficados de modulo usuarios

--- src/pages/AñadirRol.jsx
+++ src/pages/AñadirRol.jsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import Fijo from '../components/Fijo'
 import NavUsuarios from '../components/NavUsuarios'
 import { useState } from "react";
@@ -50,16 +49,198 @@ export default function AñadirRol() {
               <br />
               <br />
               <div class="checkbox-group">
-                <input type="checkbox" /> Usuarios
+                <input type="checkbox" onClick={() => toggleSubMenu('permisos-usuarios')} /> Usuarios
+                <input type="checkbox" onClick={() => toggleSubMenu('permisos-compras')} /> Compras
+                <input type="checkbox" onClick={() => toggleSubMenu('permisos-productos')} /> Productos
                 <input type="checkbox" onClick={() => toggleSubMenu('permisos-ventas')} /> Ventas
-                <input type="checkbox" onClick={() => toggleSubMenu('permisos-sig')} /> SIG
               </div>
+              <br />
+              <div className="section dropdown" id='permisos-usuarios'>
+                <h3>Permisos módulo usuarios</h3>
+                <br />
+                <div class="permissions">
+                  <div className="group">
+                    <label >
+                      <input style={{ marginRight: '0.5rem', marginBottom: '.5rem' }} type="checkbox" onChange={() => toggleSubMenu('lista-usuarios')} />
+                      Lista de usuarios
+                    </label>
+                    <br />
+                    <label>
+                      <input style={{ marginRight: '0.5rem', marginBottom: '.5rem' }} type="checkbox" />
+                      Añadir usuario
+                    </label>
+                    <br />
 
+                  </div>
+                  <div className="group">
+                    <label>
+                      <input style={{ marginRight: '0.5rem', marginBottom: '.5rem' }} type="checkbox" />
+                      Añadir rol
+                    </label>
+                    <br />
+
+                    <label>
+                      <input style={{ marginRight: '0.5rem', marginBottom: '.5rem' }} type="radio" />
+                      Todos
+                    </label>
+                  </div>
+                  <br />
+
+                </div>
+                <br />
+                <div class="form-group-rol dropdown" id='lista-usuarios'>
+                  <label>Permisos para lista de usuarios</label>
+                  <div class="radio-options">
+                    <input type="radio" name="usersListPermissions" /> Solo ver
+                    <input type="checkbox" /> Editar usuarios
+                    <input type="checkbox" /> Habilitar / Inhabilitar
+                    <input type="checkbox" /> Eliminar usuarios
+                    <input type="radio" name="usersListPermissions"/> Todos los permisos
+                  </div>
+                </div>
+
+              </div>
+              <div className="section dropdown" id='permisos-compras'>
+                <h3>Permisos módulo compras</h3>
+                <br />
+                <div class="permissions">
+                  <div className="group">
+                    <label>
+                      <input style={{ marginRight: '0.5rem', marginBottom: '.5rem' }} type="checkbox" />
+                      Lista de proveedores
+                    </label>
+                    <br />
+                    <label>
+                      <input style={{ marginRight: '0.5rem', marginBottom: '.5rem' }} type="checkbox" name="entregados" checked={selected.entregados} onChange={handleChange} />
+                      Historial de compras
+                    </label>
+                    <br />
+                  </div>
+                  <div className="group">
+                    <label>
+                      <input style={{ marginRight: '0.5rem', marginBottom: '.5rem' }} type="checkbox" name="cancelados" checked={selected.cancelados} onChange={handleChange} />
+                      Ver reportes
+                    </label>
+                    <br />
+
+                    <label>
+                      <input style={{ marginRight: '0.5rem', marginBottom: '.5rem' }} type="radio" />
+                      Todos
+                    </label>
+                  </div>
+                </div>
+                <br />
+
+              </div>
+              <div className="section dropdown" id='permisos-productos'>
+                <h3>Permisos módulo productos</h3>
+                <br />
+
+                <div class="permissions">
+
+                  <div className="group">
+                    <label>
+                      <input style={{ marginRight: '0.5rem', marginBottom: '.5rem' }} type="checkbox" />
+                      Lista de productos
+                    </label>
+                    <br />
+                  </div>
+                  <div className="group">
+                    <label>
+                      <input style={{ marginRight: '0.5rem', marginBottom: '.5rem' }} type="checkbox" name="cancelados" checked={selected.cancelados} onChange={handleChange} />
+                      Pedidos cancelados
+                    </label>
+                    <br />
+                    <label>
+                      <input style={{ marginRight: '0.5rem', marginBottom: '.5rem' }} type="checkbox" name="cotizacion" checked={selected.cotizacion} onChange={handleChange} />
+                      Registrar cotización
+                    </label>
+                    <br />
+                    <label>
+                      <input style={{ marginRight: '0.5rem', marginBottom: '.5rem' }} type="checkbox" name="listaCotizaciones" checked={selected.listaCotizaciones} onChange={handleChange} />
+                      Lista de cotizaciones
+                    </label>
+                    <br />
+                    <label>
+                      <input style={{ marginRight: '0.5rem', marginBottom: '.5rem' }} type="radio" name="todos" onClick={() => toggleCheckboxes(setSelected)} />
+                      Todos
+                    </label>
+                  </div>
+                </div>
+                <br />
+
+                <div class="form-group-rol">
+                  <label>Permisos para pedidos agendados</label>
+                  <div class="radio-options">
+                    <div>
+                      <input type="radio" /> Solo ver
+                    </div>
+                    <div>
+                      <input type="radio" /> Todos los permisos
+                    </div>
+                  </div>
+                </div>
+                <div class="form-group-rol">
+                  <label>Permisos para pedidos entregados</label>
+                  <div class="radio-options">
+                    <div>
+                      <input type="radio" /> Solo ver
+                    </div>
+                    <div>
+                      <input type="radio"/> Todos los permisos
+                    </div>
+                  </div>
+                </div>
+                <div class="form-group-rol">
+                  <label>Permisos para lista de devoluciones</label>
+                  <div class="radio-options">
+                    <div>
+                      <input type="radio"  /> Solo ver
+                    </div>
+                    <div>
+                      <input type="radio" name="pedidosagendados" /> Todos los permisos
+                    </div>
+                  </div>
+                </div>
+                <div class="form-group-rol">
+                  <label>Permisos para lista de cotizaciones</label>
+                  <div class="radio-options">
+                    <div>
+                      <input type="radio" name="pedidosagendados" /> Solo ver
+                    </div>
+                    <div>
+                      <input type="radio" name="pedidosagendados" /> Todos los permisos
+                    </div>
+                  </div>
+                </div>
+                <div class="form-group-rol">
+                  <label>Permisos para lista de clientes</label>
+                  <div class="radio-options">
+                    <div>
+                      <input type="radio" name="pedidosagendados" /> Solo ver
+                    </div>
+                    <div>
+                      <input type="radio" name="pedidosagendados" /> Todos los permisos
+                    </div>
+                  </div>
+                </div>
+                <div class="form-group-rol">
+                  <label>Permisos para prospectos de cliente</label>
+                  <div class="radio-options">
+                    <div>
+                      <input type="radio" name="pedidosagendados" /> Solo ver
+                    </div>
+                    <div>
+                      <input type="radio" name="pedidosagendados" /> Todos los permisos
+                    </div>
+                  </div>
+                </div>
+              </div>
               <div className="section dropdown" id='permisos-ventas'>
                 <h3>Permisos módulo ventas</h3>
                 <br />
-                <div class="permissions">
 
+                <div class="permissions">
 
                   <div className="group">
                     <label>
@@ -173,113 +354,11 @@ export default function AñadirRol() {
                   </div>
                 </div>
               </div>
-              <div class="section dropdown" id='permisos-sig'>
-              <h3>Permisos módulo SIG</h3><br />
-              <div class="form-group-rol">
-                <label>Permisos para agregar documentos</label>
-                <div class="radio-options">
-                  <div>
-                    <input type="radio" name="agregar-doc" /> Sí
-                  </div>
-                  <div>
-                    <input type="radio" name="agregar-doc" /> No
-                  </div>
-                </div>
+              <div className="buttons">
+                <Link to={`/AñadirRol`} onClick={handleClick}>
+                  <button className="btn btn-primary">Crear Rol</button>
+                </Link>
               </div>
-              <div class="form-group-rol">
-                <label>Permisos para editar documentos</label>
-                <div class="radio-options">
-                  <div>
-                    <input type="radio" name="agregar-doc" /> Sí
-                  </div>
-                  <div>
-                    <input type="radio" name="agregar-doc" /> No
-                  </div>
-                </div>
-              </div>
-              <div class="form-group-rol">
-                <label>Permisos para eliminar documentos</label>
-                <div class="radio-options">
-                  <div>
-                    <input type="radio" name="agregar-doc" /> Sí
-                  </div>
-                  <div>
-                    <input type="radio" name="agregar-doc" /> No
-                  </div>
-                </div>
-              </div>
-              <div class="form-group-rol">
-                <label>Permisos para añadir procesos</label>
-                <div class="radio-options">
-                  <div>
-                    <input type="radio" name="agregar-doc" /> Sí
-                  </div>
-                  <div>
-                    <input type="radio" name="agregar-doc" /> No
-                  </div>
-                </div>
-              </div>
-              <div class="form-group-rol">
-                <label>Permisos para editar procesos</label>
-                <div class="radio-options">
-                  <div>
-                    <input type="radio" name="agregar-doc" /> Sí
-                  </div>
-                  <div>
-                    <input type="radio" name="agregar-doc" /> No
-                  </div>
-                </div>
-              </div>
-              <div class="form-group-rol">
-                <label>Permisos para eliminar procesos</label>
-                <div class="radio-options">
-                  <div>
-                    <input type="radio" name="agregar-doc" /> Sí
-                  </div>
-                  <div>
-                    <input type="radio" name="agregar-doc" /> No
-                  </div>
-                </div>
-              </div>
-              <div class="form-group-rol">
-                <label>Permisos para añadir tipos de procesos</label>
-                <div class="radio-options">
-                  <div>
-                    <input type="radio" name="agregar-doc" /> Sí
-                  </div>
-                  <div>
-                    <input type="radio" name="agregar-doc" /> No
-                  </div>
-                </div>
-              </div>
-              <div class="form-group-rol">
-                <label>Permisos para editar tipos de procesos</label>
-                <div class="radio-options">
-                  <div>
-                    <input type="radio" name="agregar-doc" /> Sí
-                  </div>
-                  <div>
-                    <input type="radio" name="agregar-doc" /> No
-                  </div>
-                </div>
-              </div>
-              <div class="form-group-rol">
-                <label>Permisos para eliminar tipos de procesos</label>
-                <div class="radio-options">
-                  <div>
-                    <input type="radio" name="agregar-doc" /> Sí
-                  </div>
-                  <div>
-                    <input type="radio" name="agregar-doc" /> No
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div className="buttons">
-              <Link to={`/AñadirRol`} onClick={handleClick}>
-                <button className="btn btn-primary">Crear Rol</button>
-              </Link>
-            </div>
             </form>
 
           </div>
