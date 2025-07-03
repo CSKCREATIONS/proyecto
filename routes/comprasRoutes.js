08641605307f49eda648ae5e6c221e15be1559b7
SFB009 <148232633+sHari7860@users.noreply.github.com>
2025-07-03 16:28:51 -0500

    Delete routes directory

--- routes/comprasRoutes.js
+++
@@ -1,9 +0,0 @@
-// routes/compraRoutes.js
-const express = require('express');
-const router = express.Router();
-const compraController = require('../controllers/compraController');
-
-router.post('/', compraController.crearCompra);
-router.get('/', compraController.obtenerCompras);
-
-module.exports = router;
