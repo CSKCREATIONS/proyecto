3c1f87d640ead106547a2b3ee6a6d6badf9496cc
SFB009 <148232633+sHari7860@users.noreply.github.com>
2025-07-03 16:28:15 -0500

    Delete front1 directory

--- front1/src/components/EncabezadoModulo2.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function EncabezadoModulo2(props) {
-  return (
-    <div className='encabezado_modulo2'>
-      <h3>{props.titulo}</h3>
-    </div>
-  )
-}
