27f0c3ed565cf0b9c8acf5d5a227ac5a97cfafb6
SFB009 <148232633+sHari7860@users.noreply.github.com>
2025-07-03 16:27:56 -0500

    Delete config directory

--- config/auth.config.js
+++
@@ -1,9 +0,0 @@
-require('dotenv').config();
-
-module.exports = {
-    secret:process.env.AUTH_SECRET || "tusecretoparalostoken",
-    jwtExpiration: process.env. JWT_EXPIRATION || 86400, //24 horas en segundos
-
-    saltRounds: process.env.SALT_ROUNDS || 8
-
-};
