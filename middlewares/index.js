d37522e7178dbdf48eb01e69ad831de489975ef5
SFB009 <148232633+sHari7860@users.noreply.github.com>
2025-07-03 16:28:32 -0500

    Delete middlewares directory

--- middlewares/index.js
+++
@@ -1,9 +0,0 @@
-const authJwt = require('./auth');
-const verifySignUp = require('./verifySignUp');
-const role = require('./role');
-
-module.exports = {
-    authJwt: require('./authJwt'),
-    verifySignUp: require('./verifySignUp'),
-    role: require('./role')
-};
